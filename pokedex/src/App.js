856af5f16507947de68f3a5d877988c74711a0e4
Sajal Dubey <dubeysajal@gmail.com>
2022-10-09 14:21:17 +0530

    pokedex excercise

---
+++ pokedex/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+import Pokecard from "./Pokecard";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Pokecard />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ pokedex/src/Pokecard.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Pokecard extends Component {
+  render() {
+    return (
+      <div>
+        <p>Inside Pokecard Component!!!</p>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
